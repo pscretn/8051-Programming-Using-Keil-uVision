1dca8e2cf13768c53bc0a46fb419f130f299800d
PAUL SABU <paulsabu.123@gmail.com>
2021-04-16 22:24:44 +0530

    Create Largest.asm

---
+++ Largest_or_Smallest_of_a_Series/Largest.asm
@@ -0,0 +1,17 @@
+$mod51
+org 00h
+mov dptr ,#4300h
+mov r0,#0ah
+mov b,#00h
+mov a,#00h
+loop:movx a,@dptr
+cjne a,b,l1
+l1:jc l2
+mov b,a
+l2:inc dptr
+djnz r0,loop
+mov a,b
+mov dptr,#4500h
+movx @dptr,a
+l3:sjmp l3
+end
