58b8f83be581595275a53ba9407fb319f0d7adf7
PAUL SABU <paulsabu.123@gmail.com>
2021-04-21 10:40:01 +0530

    File

---
+++ Sorting-of-data(Ascending & Descending)/Ascending.asm
@@ -0,0 +1,30 @@
+$mod51
+org 00h
+mov r4,#0ah
+l3:mov r3,#09h
+mov dptr,#4300h
+clr c
+l2:movx a,@dptr
+mov r1,a
+inc dptr
+movx a,@dptr
+subb a,r1
+jnc l1
+movx a,@dptr
+dec dpl
+movx @dptr,a
+mov a,r1
+inc dptr 
+movx @dptr,a
+l1:djnz r3,l2
+djnz r4,l3
+mov dptr,#4300h
+movx a,@dptr
+mov dptr,#4350h
+movx @dptr,a
+mov dptr,#4309h
+movx a,@dptr
+mov dptr,#4351h
+movx @dptr,a
+l:sjmp l
+end
