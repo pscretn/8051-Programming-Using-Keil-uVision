07fab3f6fd45116867ae24fafc262df2ad2e0a46
PAUL SABU <paulsabu.123@gmail.com>
2021-04-27 17:26:53 +0530

    code

---
+++ Arithemetic-Operations/add_8-bit.asm
@@ -0,0 +1,18 @@
+$mod51
+org 00h
+mov dptr,#4300h
+movx a,@dptr
+mov r1,a
+inc dptr
+movx a,@dptr
+mov r0, #00h
+add a,r1
+mov dptr,#4500h
+movx @dptr,a
+jnc l1
+inc r0
+l1:inc dptr
+mov a,r0
+movx @dptr,a
+l2:sjmp l2
+end
